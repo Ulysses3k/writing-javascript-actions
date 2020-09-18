698497ebbccfee61821bd6156b78cf3aa9669432
Costas <ceconomoujr@gmail.com>
2020-09-18 17:56:49 +0300

    Experiment 2 Fix

--- .github/actions/hello-world/main.js
+++ .github/actions/hello-world/main.js
@@ -2,7 +2,7 @@ const core = require("@actions/core");
 
 const firstGreeting = core.getInput("first-greeting");
 const secondGreeting = core.getInput("second-greeting");
-const thirdGreeting = core.getInput("thrid-greeting");
+const thirdGreeting = core.getInput("third-greeting");
 
 console.log(`Hello ${firstGreeting}`);
 console.log(`Hello ${secondGreeting}`);
